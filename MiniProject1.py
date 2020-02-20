8aa6f887de6aef9cf58a2d1c5afb2d04126a58d4
AmandaLescano <60947243+AmandaLescano@users.noreply.github.com>
2020-02-20 18:15:01 -0500

    Update MiniProject1.py

--- MiniProject1.py
+++ MiniProject1.py
@@ -4,19 +4,26 @@ Created on Thu Feb 20 15:53:37 2020
 
 @author: amand
 """
-
 from random import randint
 import pandas as pd
 
 df = pd.read_csv('miniqbank.csv')
 
 
-engineering_bank = df[df['Category'] == 'Engineering for Dummies']
-rosenberg_bank = df[df['Category'] == 'Rosenberg Wisdom']
-ru_bank = df[df['Category'] == 'R-U-RAH-RAH']
-new_jersey_bank = df[df['Category'] == 'NeW jErSeY']
-old_shows_bank = df[df['Category'] == 'Old TV Shows']
-sports_bank = df[df['Category'] == 'Sports']
+engineering_bank = df[df['category'] == 'Engineering for Dummies']
+rosenberg_bank = df[df['category'] == 'Rosenberg Wisdom']
+ru_bank = df[df['category'] == 'R-U-RAH-RAH']
+new_jersey_bank = df[df['category'] == 'NeW jErSeY']
+old_shows_bank = df[df['category'] == 'Old TV Shows']
+sports_bank = df[df['category'] == 'Sports']
+#players = int(input('How many players?:')
+
+#def determine_players(players):   
+#    if players in range(2,5):
+#       return players
+#   else:
+#       print('Incorrent amount of players. Please enter a number between 2 and 4')
+#       players = int(input('How many players?:'))
 
 
 def determine_color():
@@ -62,18 +69,17 @@ def get_correct_bank(category, engineering_bank, rosenberg_bank, ru_bank, new_je
         return rosenberg_bank
     elif category == 'R-U-RAH-RAH':
          #return random question from RU bank
-        return
+        return ru_bank
     elif category == 'NeW jErSeY':
          #return random question from NJ bank
-        return
+        return new_jersey_bank
     elif category == 'Old TV Shows':
          #return random question from TV bank
-        return
+        return old_shows_bank
     else: 
         #return random question from Sports bank
-        return
+        return sports_bank
         
-#finish this above   
     
 def select_question(correct_bank):
     question = correct_bank.sample(n=1)
@@ -82,6 +88,9 @@ def select_question(correct_bank):
 def display_q(question):
     print(question['question'].iloc(0)) #change this
     return
+
+#def whose_turn():        
+#    return player
 
 def get_guess():
     guess = input('Answer here:')
@@ -109,6 +118,7 @@ def remove_question(my_bank, my_q):
 #    return sad
         
 def play_single_turn():
+#    num_of_players = determine_players()
     color = determine_color()
     category = determine_category(color)
     my_bank = get_correct_bank(category,engineering_bank, rosenberg_bank, ru_bank, new_jersey_bank,old_shows_bank,sports_bank)
